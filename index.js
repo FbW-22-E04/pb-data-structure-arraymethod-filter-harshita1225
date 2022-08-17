3478b5601e4457078a5340de53a0c49a8fa6fe3f
harshita <harshita1225@gmail.com>
2022-08-17 19:45:29 +0200

    task completed

--- index.js
+++ index.js
@@ -2,25 +2,32 @@
 //1
 
 function filterRange(arr, a, b) {
-    //write your code here
-  }
-  
-  let arr = [5, 3, 8, 1];
-  
-  let filtered = filterRange(arr, 1, 4);
-  
-  console.log( filtered ); // 3,1 (matching values)
-  
-  console.log( arr ); // 5,3,8,1 (not modified)
+  const result = arr.filter((value) => value >= a && value <= b);
+  return result;
+}
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+console.log(filtered); // 3,1 (matching values)
+
+console.log(arr); // 5,3,8,1 (not modified)
 
 //Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 //2
-  function filterRangeInPlace(arr, a, b) {
-    //write your code here
+function filterRangeInPlace(arr, a, b) {
+  let newArray = [];
+  for (const [i, element] of arr.entries()) {
+    if (element < a || element > b) {
+      newArray = arr.splice(i, 1);
+    }
   }
-  
-  let array = [5, 3, 8, 1];
-  
-  filterRangeInPlace(array, 1, 4); // removed the numbers except from 1 to 4
-  
-  console.log( array ); // [3, 1]
+  return newArray;
+}
+
+let array = [5, 3, 8, 1]; // removed the numbers except from 1 to 4
+
+filterRangeInPlace(array, 1, 4); // [3, 1]
+
+console.log(array); //[3,1]
